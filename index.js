4bf01ea9c8db1f5af2bb543e1fdae556e4894f0f
PARSIVALJ <uchejesusboy@gmail.com>
2023-09-17 09:30:49 +0300

    removed errors from index.js

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-document.querySelector("img").setAttribute("src", "./images/dice"+randomNumber1+".png");
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(".img1").setAttribute("src", "./images/dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src", "./images/dice"+randomNumber2+".png");
